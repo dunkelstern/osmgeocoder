ef683d045a282d8647500646b46c6eecefa2add3
Johannes Schriewer <hallo@dunkelstern.de>
2019-03-21 00:31:27 +0100

    Bugfix: typo

--- osmgeocoder/data/sql/optimize/003-manual_city_name_updates.sql
+++ osmgeocoder/data/sql/optimize/003-manual_city_name_updates.sql
@@ -1,3 +1,3 @@
-UPDATE public.city SET city = 'Bucharest' WHERE license_id = (SELECT id FROM license WHERE source = 'ro/bucharest' LIMIT 1);
-UPDATE public.city SET city = 'Wien' WHERE license_id = (SELECT id FROM license WHERE source = 'at/city_of_vienna' LIMIT 1);
-UPDATE public.city SET city = 'Köln' WHERE license_id = (SELECT id FROM license WHERE source = 'de/nw/city_of_cologne' LIMIT 1);
+UPDATE public.oa_city SET city = 'Bucharest' WHERE license_id = (SELECT id FROM public.oa_license WHERE source = 'ro/bucharest' LIMIT 1);
+UPDATE public.oa_city SET city = 'Wien' WHERE license_id = (SELECT id FROM public.oa_license WHERE source = 'at/city_of_vienna' LIMIT 1);
+UPDATE public.oa_city SET city = 'Köln' WHERE license_id = (SELECT id FROM public.oa_license WHERE source = 'de/nw/city_of_cologne' LIMIT 1);
