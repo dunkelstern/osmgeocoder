41657a2a4bed7eca7e81a92c6ab46cd02ef28b85
Johannes Schriewer <hallo@dunkelstern.de>
2019-03-22 21:45:00 +0100

    Optimize geocoding query for OSM

--- osmgeocoder/data/sql/geocoder/004-reverse_geocoding.sql
+++ osmgeocoder/data/sql/geocoder/004-reverse_geocoding.sql
@@ -6,23 +6,23 @@ DROP FUNCTION IF EXISTS public.point_to_address_oa(point geometry(point), radius
 CREATE OR REPLACE FUNCTION public.point_to_address_oa(point geometry(point), radius float)
 RETURNS SETOF address_and_distance AS
 $$
-	SELECT
-		h.name AS house,
-		s.street as road,
+    SELECT
+        h.name AS house,
+        s.street as road,
         h.housenumber as house_number,
         c.postcode,
-		c.city,
-		location,
-		ST_Distance(location, point) as distance,
-		c.license_id
-	FROM public.oa_house h
-	JOIN public.oa_street s ON h.street_id = s.id
-	JOIN public.oa_city c ON s.city_id = c.id
-	WHERE
-		ST_X(location) >= ST_X(point) - radius
-		AND ST_X(location) <= ST_X(point) + radius
-		AND ST_DWithin(location, point, radius) -- only search within radius
-	ORDER BY ST_Distance(location, point) -- order by distance to point
+        c.city,
+        location,
+        ST_Distance(location, point) as distance,
+        c.license_id
+    FROM public.oa_house h
+    JOIN public.oa_street s ON h.street_id = s.id
+    JOIN public.oa_city c ON s.city_id = c.id
+    WHERE
+        ST_X(location) >= ST_X(point) - radius
+        AND ST_X(location) <= ST_X(point) + radius
+        AND ST_DWithin(location, point, radius) -- only search within radius
+    ORDER BY ST_Distance(location, point) -- order by distance to point
 $$ LANGUAGE 'sql';
 
 --
@@ -33,23 +33,21 @@ DROP FUNCTION IF EXISTS point_to_address_osm(point geometry(point), radius float
 CREATE OR REPLACE FUNCTION point_to_address_osm(point geometry(point), radius float)
 RETURNS SETOF address_and_distance AS
 $$
-	SELECT
-		'' AS house,
-		s.name as road,
+    SELECT
+        '' AS house,
+        s.name as road,
         h.house_number,
         c.postcode,
-		c.name as city,
-		h.geometry as location,
-		ST_Distance(h.geometry, point) as distance,
-		(SELECT id FROM oa_license WHERE source = 'osm' LIMIT 1) as license_id
-	FROM public.osm_struct_house h
-	JOIN public.osm_struct_street s ON h.street_id = s.id
-	JOIN public.osm_struct_city c ON s.city_id = c.id
-	WHERE
-		ST_X(h.geometry) >= ST_X(point) - radius
-		AND ST_X(h.geometry) <= ST_X(point) + radius
-		AND ST_DWithin(h.geometry, point, radius) -- only search within radius
-	ORDER BY ST_Distance(h.geometry, point) -- order by distance to point
+        c.name as city,
+        h.geometry as location,
+        ST_Distance(h.geometry, point) as distance,
+        (SELECT id FROM oa_license WHERE source = 'osm' LIMIT 1) as license_id
+    FROM public.osm_struct_house h
+    JOIN public.osm_struct_streets s ON h.street_id = s.id
+    JOIN public.osm_struct_cities c ON s.city_id = c.id
+    WHERE
+        ST_DWithin(h.geometry, point, radius) -- only search within radius
+    ORDER BY ST_Distance(h.geometry, point) -- order by distance to point
 $$ LANGUAGE 'sql';
 
 -- SELECT * FROM point_to_address(ST_Transform(ST_SetSRID(ST_MakePoint(9.738889, 47.550535), 4326), 3857), 250) LIMIT 10;
